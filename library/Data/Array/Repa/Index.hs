b4d04fb602dde7a320fd8ca8a9e38c3b271f990c
benl <benl@ouroborus.net>
2010-04-12 17:10:57 +1000

    Add to tests, and arbitraryShape
    Ignore-this: bbbbb859ae11eef47e8d4da3f0b4b2d5
    
    darcs-hash:20100412071057-bf82d-01f1e291573afde9c9cd0f92d90f8a0da9d9b748

--- library/Data/Array/Repa/Index.hs
+++ library/Data/Array/Repa/Index.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeOperators, FlexibleInstances #-}
+{-# LANGUAGE TypeOperators, FlexibleInstances, ScopedTypeVariables #-}
 
 module Data.Array.Repa.Index
 	( Z	(..)
@@ -8,13 +8,18 @@ module Data.Array.Repa.Index
 	, DIM2
 	, DIM3
 	, DIM4
-	, DIM5 )
+	, DIM5 
+	, tests_DataArrayRepaIndex)
 where
+import Data.Array.Repa.Shape
 import Test.QuickCheck
+import Control.Monad
+import GHC.Base 		(quotInt, remInt)
 
 
 -- | An index of dimension zero
 data Z	= Z
+	deriving (Show, Eq, Ord)
 
 -- | Our index type, used for both shapes and indices.
 infixl 3 :.
@@ -31,9 +36,145 @@ type DIM4	= DIM3 :. Int
 type DIM5	= DIM4 :. Int
 
 
--- Instances --------------------------------------------------------------------------------------
-instance Arbitrary sh => Arbitrary (sh :. Int) where
+-- Shape ------------------------------------------------------------------------------------------
+instance Shape Z where
+	dim _			= 0
+	zeroDim			= Z
+	unitDim			= Z
+	size _			= 1
+	sizeIsValid _		= True
+	toIndex _ _		= 0
+	fromIndex _ _		= Z
+
+	inRange Z Z Z		= True
+	intersectDim _ _	= Z
+	next _ _		= Nothing
+
+	deepSeq Z x		= x
+
+	
+instance Shape sh => Shape (sh :. Int) where
+	{-# INLINE dim #-}
+	dim   (sh  :. _)
+		= dim sh + 1
+
+	{-# INLINE zeroDim #-}
+	zeroDim = zeroDim :. 0
+
+	{-# INLINE unitDim #-}
+	unitDim = unitDim :. 1
+
+	{-# INLINE size #-}
+	size  (sh1 :. n)
+		= size sh1 * n
+
+	{-# INLINE sizeIsValid #-}
+	sizeIsValid (sh1 :. n)
+		| size sh1 > 0
+		= n <= maxBound `div` size sh1
+		
+		| otherwise
+		= False
+		
+	{-# INLINE toIndex #-}
+	toIndex (sh1 :. sh2) (sh1' :. sh2') 
+		= toIndex sh1 sh1' * sh2 + sh2'
+
+	{-# INLINE fromIndex #-}
+	fromIndex (ds :. d) n 
+	 	= fromIndex ds (n `quotInt` d) :. r
+		where
+		-- If we assume that the index is in range, there is no point
+		-- in computing the remainder for the highest dimension since
+		-- n < d must hold. This saves one remInt per element access which
+		-- is quite a big deal.
+		r 	| dim ds == 0	= n
+			| otherwise	= n `remInt` d
+
+	{-# INLINE inRange #-}
+	inRange (zs :. z) (sh1 :. n1) (sh2 :. n2) 
+		= (n2 >= z) && (n2 < n1) && (inRange zs sh1 sh2)
+
+	{-# INLINE intersectDim #-}
+	intersectDim (sh1 :. n1) (sh2 :. n2) 
+		= (intersectDim sh1 sh2 :. (min n1 n2))
+
+	{-# INLINE next #-}
+	next  sh@(sh' :. s) msh@(msh' :. ms) 
+		| sh == msh     = Nothing
+		| s  < (ms-1)   = Just (sh' :. (s+1))    
+		| otherwise 
+		= case next sh' msh' of
+			Just shNext -> Just (shNext :. 0)
+			Nothing     -> Nothing
+           
+	{-# INLINE deepSeq #-} 
+	deepSeq (sh :. n) x = deepSeq sh (n `seq` x)
+
+
+
+
+-- Arbitrary --------------------------------------------------------------------------------------
+instance Arbitrary Z where
+	arbitrary	= return Z
+
+-- | Generate an arbitrary index, which may have 0's for some components.
+instance (Shape sh, Arbitrary sh) => Arbitrary (sh :. Int)  where
 	arbitrary 
-	 = do	h	<- arbitrary
-		i	<- arbitrary
-		return	$ h :. i
+	 = do	sh1		<- arbitrary
+		
+		-- Make sure not to create an index so big that we get
+		--	integer overflow when converting it to the linear form.
+		n		<- liftM abs $ arbitrary
+		let nMax	= maxBound `div` (size sh1)
+		let nMaxed	= n `mod` nMax
+		
+		return	$ sh1 :. nMaxed 
+
+
+-- | Generate an aribrary shape that does not have 0's for any component.
+arbitraryShape 
+	:: (Shape sh, Arbitrary sh) 
+	=> Gen (sh :. Int)
+
+arbitraryShape 
+ = do	sh1		<- arbitrary
+	let sh1Unit	= if sh1 == zeroDim then unitDim else sh1
+
+	-- Make sure not to create an index so big that we get
+	--	integer overflow when converting it to the linear form.
+	n		<- liftM abs $ arbitrary
+	let nMax	= maxBound `div` size sh1Unit
+	let nMaxed	= n `mod` nMax
+	let nClamped	= if nMaxed == 0 then 1 else nMaxed
+	
+	return $ sh1Unit :. nClamped
+	
+
+
+genInShape2 :: DIM2 -> Gen DIM2
+genInShape2 sh@(Z :. yMax :. xMax)
+ = do	y	<- liftM (`mod` yMax) $ arbitrary
+	x	<- liftM (`mod` xMax) $ arbitrary
+	return	$ Z :. y :. x
+	
+
+-- Tests ------------------------------------------------------------------------------------------
+tests_DataArrayRepaIndex
+ = 	[ ("toIndexFromIndex/DIM1", property prop_DIM1_toIndexFromIndex) 
+	, ("toIndexFromIndex/DIM2", property prop_DIM2_toIndexFromIndex) ]
+
+prop_DIM1_toIndexFromIndex sh ix
+	=   (sizeIsValid sh)
+	==> (inShape sh ix)
+	==> fromIndex sh (toIndex sh ix) == ix
+	where	_types	= ( sh :: DIM1
+			  , ix :: DIM1)
+
+prop_DIM2_toIndexFromIndex 
+ =	forAll arbitraryShape   $ \(sh :: DIM2) ->
+   	forAll (genInShape2 sh) $ \(ix :: DIM2) ->
+	fromIndex sh (toIndex sh ix) == ix
+
+
+
--- library/Data/Array/Repa/Shape.hs
+++ library/Data/Array/Repa/Shape.hs
@@ -2,18 +2,13 @@
 
 -- | Class of types that can be used as array shapes and indices.
 module Data.Array.Repa.Shape
-	( Shape(..)
-	, tests_DataArrayRepaShape)
+	(Shape(..))
 where
-import Data.Array.Repa.Index
-import Test.Framework
-import Test.Framework.Providers.QuickCheck2
-import GHC.Base 				(quotInt, remInt)
+import Test.QuickCheck.Arbitrary
 	
-
 -- Shape ------------------------------------------------------------------------------------------	
 -- | Class of types that can be used as array shapes and indices.
-class (Show sh, Eq sh) => Shape sh where
+class (Eq sh, Arbitrary sh) => Shape sh where
 
 	-- | Get the number of dimensions in a `Shape`.
 	dim	:: sh -> Int           
@@ -21,8 +16,14 @@ class (Show sh, Eq sh) => Shape sh where
 	-- | The shape of an array of size zero, with a particular dimensionality.
 	zeroDim	:: sh
 
+	unitDim :: sh
+
 	-- | Get the total number of elements in an array with this `Shape`.
 	size	:: sh -> Int           
+
+	-- | Check whether the size of this shape is small enough to be represented
+	--	as an integer index.
+	sizeIsValid :: sh -> Bool
 
 	-- | Compute an index into the linear representation of an array.
 	toIndex :: sh	-- ^ Shape of the array.
@@ -56,78 +57,11 @@ class (Show sh, Eq sh) => Shape sh where
 	deepSeq :: sh -> a -> a
 
 
--- Instances --------------------------------------------------------------------------------------
-instance Shape () where
-	dim _			= 0
-	size _			= 1
-	toIndex _ _		= 0
-	fromIndex _ _		= ()
+	-- | Check whether an index is a part of a given shape.
+	inShape :: sh	-- ^ Shape.
+		-> sh	-- ^ Index.
+		-> Bool
 
-	inRange () () ()	= True
-	zeroDim			= ()
-	intersectDim _ _	= ()
-	next _ _		= Nothing
-
-	deepSeq () x		= x
-
-
-instance Shape sh => Shape (sh :. Int) where
-	{-# INLINE dim #-}
-	dim   (sh  :. _)
-		= dim sh + 1
-
-	{-# INLINE size #-}
-	size  (sh1 :. n)
-		= size sh1 * n
-
-	{-# INLINE toIndex #-}
-	toIndex (sh1 :. sh2) (sh1' :. sh2') 
-		= toIndex sh1 sh1' * sh2 + sh2'
-
-	{-# INLINE fromIndex #-}
-	fromIndex (ds :. d) n 
-	 	= fromIndex ds (n `quotInt` d) :. r
-		where
-		-- If we assume that the index is in range, there is no point
-		-- in computing the remainder for the highest dimension since
-		-- n < d must hold. This saves one remInt per element access which
-		-- is quite a big deal.
-		r 	| dim ds == 0	= n
-			| otherwise	= n `remInt` d
-
-	{-# INLINE inRange #-}
-	inRange (zs :. z) (sh1 :. n1) (sh2 :. n2) 
-		= (n2 >= z) && (n2 < n1) && (inRange zs sh1 sh2)
-
-	{-# INLINE zeroDim #-}
-	zeroDim = (zeroDim :. 0)
-
-	{-# INLINE intersectDim #-}
-	intersectDim (sh1 :. n1) (sh2 :. n2) 
-		= (intersectDim sh1 sh2 :. (min n1 n2))
-
-	{-# INLINE next #-}
-	next  sh@(sh' :. s) msh@(msh' :. ms) 
-		| sh == msh     = Nothing
-		| s  < (ms-1)   = Just (sh' :. (s+1))    
-		| otherwise 
-		= case next sh' msh' of
-			Just shNext -> Just (shNext :. 0)
-			Nothing     -> Nothing
-           
-	{-# INLINE deepSeq #-} 
-	deepSeq (sh :. n) x = deepSeq sh (n `seq` x)
-
-
--- Tests ------------------------------------------------------------------------------------------
-tests_DataArrayRepaShape
-	= testGroup "Data.Array.Repa.Shape"
-		[ testProperty "toIndexFromIndex" prop_Shape2_toIndexFromIndex ]
-
-prop_Shape2_toIndexFromIndex sh ix
-	= fromIndex sh (toIndex sh ix) == ix
-	where	_types	= ( sh :: (() :. Int :. Int)
-			  , ix :: (() :. Int :. Int))
-		
-		
+	inShape sh ix
+		= inRange zeroDim sh ix
 
