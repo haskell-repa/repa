7e18e27d20422e45329422a152121aec5ba1fe3f
benl <benl@ouroborus.net>
2010-04-12 19:36:02 +1000

    Add sum
    Ignore-this: 4ff6ec6bea9c2cbdaf2c98057189528a
    
    darcs-hash:20100412093602-bf82d-3ef93100ff9b0f34305dd7acf1c5cc5aff247ae5

--- library/Data/Array/Repa/Array.hs
+++ library/Data/Array/Repa/Array.hs
@@ -2,6 +2,7 @@
 
 module Data.Array.Repa.Array
 	( Array	(..)
+
 	 -- * Basic Operations
 	, force
 	, (!:)
@@ -11,8 +12,9 @@ module Data.Array.Repa.Array
 	, toList
 	
 	 -- * Computations
+	, fold
 	, zipWith
-	 
+	, sumAll
 	, tests_DataArrayRepaArray)
 where
 import Data.Array.Repa.Index
@@ -22,14 +24,17 @@ import Data.Array.Repa.Shape			(Shape)
 import Data.Array.Parallel.Unlifted		(Elt)
 import qualified Data.Array.Repa.Shape		as S
 import qualified Data.Array.Parallel.Unlifted	as U
-import Prelude					hiding (zipWith)	
+import Prelude					as P hiding (zipWith)	
 
 stage	= "Data.Array.Repa.Array"
 	
 	
 -- | Possibly delayed arrays.
 data Array sh e 
-	= Manifest sh (U.Array e)
+	= -- | An array represented as some concrete data.
+	  Manifest sh (U.Array e)
+
+          -- | An array represented as a function that computes each element.
 	| Delayed  sh (sh -> e)
 
 
@@ -37,6 +42,7 @@ data Array sh e
 
 -- | Take the shape of an `Array`.
 shape	:: Array sh e -> sh
+{-# INLINE shape #-}
 shape arr
  = case arr of
 	Manifest sh _	-> sh
@@ -45,9 +51,9 @@ shape arr
 
 -- | Force an array, so that it becomes `Manifest`.
 force	:: (Shape sh, Elt e)
-	=> Array sh e
-	-> Array sh e
+	=> Array sh e -> Array sh e
 	
+{-# INLINE force #-}
 force arr@Manifest{}	
 	= arr
 
@@ -59,11 +65,11 @@ force (Delayed sh fn)
 		(S.size sh - 1)
 
 
--- | Lookup the value in an array.
+-- | Get an indexed element from an array.
+--	WARNING: There's no bounds checking with this. 
+--               Indexing outside the array will yield badness.
 (!:) 	:: (Shape sh, Elt e)
-	=> Array sh e
-	-> sh				-- ^ Index.
-	-> e
+	=> Array sh e -> sh -> e
 
 {-# INLINE (!:) #-}
 (!:) arr ix
@@ -74,6 +80,7 @@ force (Delayed sh fn)
 
 -- | Convert a zero dimensional array into a scalar value.
 toScalar :: Elt e => Array Z e -> e
+{-# INLINE toScalar #-}
 toScalar arr
  = case arr of
 	Delayed  _ fn		-> fn Z
@@ -82,9 +89,11 @@ toScalar arr
 
 -- Conversion -------------------------------------------------------------------------------------
 -- | Convert a list to an `Array`.
---	The length of the list must be exactly the size of the shape.
+--	The length of the list must be exactly the size of the shape, 
+--	else an exception will be thrown.
 fromList 
-	:: (Shape sh, Elt e)
+	:: forall sh e
+	.  (Shape sh, Elt e)
 	=> sh				-- ^ Shape of resulting array.
 	-> [e]				-- ^ List to convert.
 	-> Array sh e
@@ -149,7 +158,7 @@ reshape arr newShape
 -- Computations ----------------------------------------------------------------------------------
 
 -- | Fold the innermost dimension. 
---	Combine with `transpose` to fold any other dimension.
+--	Combine this with `transpose` to fold any other dimension.
 fold 	:: (Elt e, Shape dim) 
 	=> (e -> e -> e) 
 	-> e 
@@ -168,7 +177,7 @@ fold f x arr
 
 -- | Combine two arrays, element-wise, with a binary operator.
 --	If the size of two array arguments differ in a dimension, the resulting
---   	array's shape is the intersection.
+--   	array's shape is their intersection.
 zipWith :: (Elt a, Elt b, Elt c, Shape sh) 
 	=> (a -> b -> c) 
 	-> Array sh a
@@ -182,10 +191,27 @@ zipWith f arr1 arr2
    in	Delayed sh' fn
 
 
+-- | Sum all the elements in the array.
+sumAll	:: (Elt e, Shape dim, Num e)
+	=> Array dim e
+	-> e
+
+sumAll arr
+	= U.fold (+) 0
+	$ U.map ((arr !:) . (S.fromIndex (shape arr)))
+	$ U.enumFromTo
+		0
+		((S.size $ shape arr) - 1)
+
+
 -- Tests ------------------------------------------------------------------------------------------
+
+-- | QuickCheck Properties.
+tests_DataArrayRepaArray :: [(String, Property)]
 tests_DataArrayRepaArray
  = 	[ ("forceIsId/DIM5",		property prop_forceIsId_DIM5)
-	, ("toListFromList/DIM3",	property prop_toListFromList_DIM3) ]
+	, ("toListFromList/DIM3",	property prop_toListFromList_DIM3) 
+	, ("sumIsSum/DIM3",		property prop_sumIsSum_DIM3) ]
 
 
 -- The Eq instance uses fold and zipWith.
@@ -200,5 +226,13 @@ prop_toListFromList_DIM3
  = 	forAll (arbitrarySmallShape 10)			$ \(sh :: DIM3) ->
 	forAll (arbitraryListOfLength (S.size sh))	$ \(xx :: [Int]) ->
 	toList (fromList sh xx) == xx
-	
+
+
+prop_sumIsSum_DIM3
+ = 	forAll (arbitrarySmallShape 100)		$ \(sh :: DIM2) ->
+	forAll (arbitraryListOfLength (S.size sh))	$ \(xx :: [Int]) -> 
+	sumAll (fromList sh xx) == P.sum xx
+
+
+
 
