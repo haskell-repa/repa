6b6a2ebe8e018e8941ea148c2493c861ec5b86e8
benl <benl@ouroborus.net>
2012-03-30 11:25:50 +1100

    Refactor gang code to reuse done mvars
    Ignore-this: 53e46d33a9cea67457df529ce3dc48fb
    
    darcs-hash:20120330002550-bf82d-a415354183e578c03268f2a65ba130767d565e20

--- repa/Data/Array/Repa/Eval/Gang.hs
+++ repa/Data/Array/Repa/Eval/Gang.hs
@@ -1,46 +1,33 @@
 {-# LANGUAGE CPP #-}
 
 -- | Gang Primitives.
---   Based on DPH code by Roman Leshchinskiy
---
-#define TRACE_GANG 0
-
 module Data.Array.Repa.Eval.Gang
-	( 
-	-- * The globally shared Repa gang
-          theGang
-
-	-- * A parallel gang of threads
-	, Gang, forkGang, gangSize, gangIO, gangST)
-	
+        ( theGang
+	, Gang, forkGang, gangSize, gangIO, gangST)	
 where
 import GHC.IO
 import GHC.ST
-import GHC.Conc                  (forkOn)
-
+import GHC.Conc                 (forkOn)
 import Control.Concurrent.MVar
-import Control.Exception         (assert)
-
-import Control.Monad             (zipWithM, zipWithM_)
+import Control.Exception        (assert)
+import Control.Monad
 import GHC.Conc			(numCapabilities)
 import System.IO
 
-#if TRACE_GANG
-import GHC.Exts                  (traceEvent)
-import System.Time ( ClockTime(..), getClockTime )
-#endif
 
--- TheGang ----------------------------------------------------------------------------------------
--- | This globally shared gang is auto-initialised at startup and shared by all Repa computations.
+-- TheGang --------------------------------------------------------------------
+-- | This globally shared gang is auto-initialised at startup and shared by all
+--   Repa computations.
 --
---   In a data parallel setting, it does not help to have multiple gangs running at the 
---   same time. This is because a single data parallel computation should already be able
---   to keep all threads busy. If we had multiple gangs running at the same time, then
---   the system as a whole would run slower as the gangs would contend for cache and thrash
---   the scheduler.
+--   In a data parallel setting, it does not help to have multiple gangs
+--   running at the same time. This is because a single data parallel
+--   computation should already be able to keep all threads busy. If we had
+--   multiple gangs running at the same time, then the system as a whole would
+--   run slower as the gangs would contend for cache and thrash the scheduler.
 --
---   If, due to laziness or otherwise, you try to start multiple parallel Repa computations
---   at the same time, then you will get the following warning on stderr at runtime:
+--   If, due to laziness or otherwise, you try to start multiple parallel
+--   Repa computations at the same time, then you will get the following
+--   warning on stderr at runtime:
 --
 -- @Data.Array.Repa: Performing nested parallel computation sequentially.
 --    You've probably called the 'compute' or 'copy' function while another
@@ -51,96 +38,54 @@ import System.Time ( ClockTime(..), getClockTime )
 --
 theGang :: Gang
 {-# NOINLINE theGang #-}
-theGang = unsafePerformIO $ forkGang numCapabilities
+theGang 
+ = unsafePerformIO 
+ $ do   let caps        = numCapabilities
+        putStr $ "forking " ++ show caps
+
+        forkGang caps
 
 
--- Requests ---------------------------------------------------------------------------------------
+-- Requests -------------------------------------------------------------------
 -- | The 'Req' type encapsulates work requests for individual members of a gang.
 data Req
-	-- | Instruct the worker to run the given action then signal it's done
-	--   by writing to the MVar.
-	= ReqDo	       (Int -> IO ()) (MVar ())
+        -- | Instruct the worker to run the given action.
+        = ReqDo	       (Int -> IO ())
 
-	-- | Tell the worker that we're shutting the gang down. The worker should
-        --   signal that it's received the request down by writing to the MVar
-        --   before returning to its caller (forkGang)
-	| ReqShutdown  (MVar ())
-
-
--- | Create a new request for the given action.
-newReq :: (Int -> IO ()) -> IO Req
-newReq p
- = do	mv	<- newEmptyMVar
-	return	$ ReqDo p mv
+	-- | Tell the worker that we're shutting the gang down.
+        --   The worker should signal that it's receieved the request by
+        --   writing to its result var before returning to the caller (forkGang).
+	| ReqShutdown
 
 
--- | Block until a thread request has been executed.
---   NOTE: only one thread can wait for the request.
-waitReq :: Req -> IO ()
-waitReq req
- = case req of
-	ReqDo     _ varDone	-> takeMVar varDone
-	ReqShutdown varDone	-> takeMVar varDone
-
-
--- Gang ------------------------------------------------------------------------------------------
+-- Gang -----------------------------------------------------------------------
 -- | A 'Gang' is a group of threads that execute arbitrary work requests.
 data Gang
-	= Gang !Int           -- Number of 'Gang' threads
-               [MVar Req]     -- One 'MVar' per thread
-               (MVar Bool)    -- Indicates whether the 'Gang' is busy
+	= Gang 
+        { -- | Number of threads in the gang.
+          _gangThreads           :: !Int           
 
+          -- | Workers listen for requests on these vars.
+        , _gangRequestVars       :: [MVar Req]     
+
+          -- | Workers put their results in these vars.
+        , _gangResultVars        :: [MVar ()] 
+
+          -- | Indicates that the gang is busy.
+        , _gangBusy              :: MVar Bool
+        } 
 
 instance Show Gang where
-  showsPrec p (Gang n _ _)
+  showsPrec p (Gang n _ _ _)
 	= showString "<<"
         . showsPrec p n
         . showString " threads>>"
 
 
--- | The worker thread of a 'Gang'.
---   The threads blocks on the MVar waiting for a work request.
-gangWorker :: Int -> MVar Req -> IO ()
-gangWorker threadId varReq
- = do	traceGang $ "Worker " ++ show threadId ++ " waiting for request."
-	req	<- takeMVar varReq
-
-	case req of
-	 ReqDo action varDone
-	  -> do	traceGang $ "Worker " ++ show threadId ++ " begin"
-		start 	<- getGangTime
-		action threadId
-		end 	<- getGangTime
-		traceGang $ "Worker " ++ show threadId ++ " end (" ++ diffTime start end ++ ")"
-
-		putMVar varDone ()
-		gangWorker threadId varReq
-
-	 ReqShutdown varDone
-	  -> do	traceGang $ "Worker " ++ show threadId ++ " shutting down."
-		putMVar varDone ()
-
-
--- | Finaliser for worker threads.
---   We want to shutdown the corresponding thread when it's MVar becomes unreachable.
---     Without this Repa programs can complain about "Blocked indefinitely on an MVar"
---     because worker threads are still blocked on the request MVars when the program ends.
---     Whether the finalizer is called or not is very racey. It happens about 1 in 10 runs
---     when for the repa-edgedetect benchmark, and less often with the others.
---
---   We're relying on the comment in System.Mem.Weak that says
---    "If there are no other threads to run, the runtime system will check for runnable
---     finalizers before declaring the system to be deadlocked."
---
---   If we were creating and destroying the gang cleanly we wouldn't need this, but theGang
---     is created with a top-level unsafePerformIO. Hacks beget hacks beget hacks...
---
-finaliseWorker :: MVar Req -> IO ()
-finaliseWorker varReq
- = do	varDone <- newEmptyMVar
-	putMVar varReq (ReqShutdown varDone)
-	takeMVar varDone
-	return ()
+-- | O(1). Yield the number of threads in the 'Gang'.
+gangSize :: Gang -> Int
+gangSize (Gang n _ _ _) 
+        = n
 
 
 -- | Fork a 'Gang' with the given number of threads (at least 1).
@@ -148,113 +93,128 @@ forkGang :: Int -> IO Gang
 forkGang n
  = assert (n > 0)
  $ do
-	-- Create the vars we'll use to issue work requests.
-	mvs	<- sequence . replicate n $ newEmptyMVar
+        -- Create the vars we'll use to issue work requests.
+        mvsRequest     <- sequence $ replicate n $ newEmptyMVar
 
-	-- Add finalisers so we can shut the workers down cleanly if they become unreachable.
-	mapM_ (\var -> addMVarFinalizer var (finaliseWorker var)) mvs
+        -- Create the vars we'll use to signal that threads are done.
+        mvsDone        <- sequence $ replicate n $ newEmptyMVar
 
-	-- Create all the worker threads
-	zipWithM_ forkOn [0..]
-		$ zipWith gangWorker [0 .. n-1] mvs
+        -- Add finalisers so we can shut the workers down cleanly if they
+        -- become unreachable.
+        zipWithM_ (\varReq varDone 
+                        -> addMVarFinalizer varReq (finaliseWorker varReq varDone)) 
+                mvsRequest
+                mvsDone
 
-	-- The gang is currently idle.
-	busy	<- newMVar False
+        -- Create all the worker threads
+        zipWithM_ forkOn [0..]
+                $ zipWith3 gangWorker 
+                        [0 .. n-1] mvsRequest mvsDone
 
-	return $ Gang n mvs busy
+        -- The gang is currently idle.
+        busy   <- newMVar False
+
+        return $ Gang n mvsRequest mvsDone busy
 
 
--- | O(1). Yield the number of threads in the 'Gang'.
-gangSize :: Gang -> Int
-gangSize (Gang n _ _) = n
+
+-- | The worker thread of a 'Gang'.
+--   The threads blocks on the MVar waiting for a work request.
+gangWorker :: Int -> MVar Req -> MVar () -> IO ()
+gangWorker threadId varRequest varDone
+ = do   
+        -- Wait for a request 
+        req	<- takeMVar varRequest
+
+	case req of
+	 ReqDo action
+	  -> do	-- Run the action we were given.
+                action threadId
+
+                -- Signal that the action is complete.
+		putMVar varDone ()
+
+                -- Wait for more requests.
+		gangWorker threadId varRequest varDone
+
+	 ReqShutdown
+	  ->    putMVar varDone ()
+
+
+-- | Finaliser for worker threads.
+--   We want to shutdown the corresponding thread when it's MVar becomes
+--   unreachable.
+--   Without this Repa programs can complain about "Blocked indefinitely
+--   on an MVar" because worker threads are still blocked on the request
+--   MVars when the program ends. Whether the finalizer is called or not
+--   is very racey. It happens about 1 in 10 runs when for the
+--   repa-edgedetect benchmark, and less often with the others.
+--
+--   We're relying on the comment in System.Mem.Weak that says
+--    "If there are no other threads to run, the runtime system will
+--     check for runnablefinalizers before declaring the system to be
+--     deadlocked."
+--
+--   If we were creating and destroying the gang cleanly we wouldn't need
+--     this, but theGang is created with a top-level unsafePerformIO.
+--     Hacks beget hacks beget hacks...
+--
+finaliseWorker :: MVar Req -> MVar () -> IO ()
+finaliseWorker varReq varDone 
+ = do   putMVar varReq ReqShutdown
+	takeMVar varDone
+	return ()
 
 
 -- | Issue work requests for the 'Gang' and wait until they complete.
 --
---   If the gang is already busy then print a warning to `stderr` and just run the 
---   actions sequentially in the requesting thread.
---
+--   If the gang is already busy then print a warning to `stderr` and just
+--   run the actions sequentially in the requesting thread.
 gangIO	:: Gang
 	-> (Int -> IO ())
 	-> IO ()
 
 {-# NOINLINE gangIO #-}
-gangIO (Gang n mvs busy) p
- = do	traceGang   "gangIO: issuing work requests (SEQ_IF_GANG_BUSY)"
-	b <- swapMVar busy True
-
-	traceGang $ "gangIO: gang is currently " ++ (if b then "busy" else "idle")
+gangIO gang@(Gang _ _ _ busy) action
+ = do   b <- swapMVar busy True
 	if b
-	 then do
-		hPutStr stderr
-		 $ unlines	[ "Data.Array.Repa: Performing nested parallel computation sequentially."
-				, "  You've probably called the 'compute' or 'copy' function while another"
-				, "  instance was already running. This can happen if the second version"
-				, "  was suspended due to lazy evaluation. Use 'deepSeqArray' to ensure"
-				, "  that each array is fully evaluated before you 'compute' the next one."
-				, "" ]
+         then do
+                seqIO gang action
 
-		mapM_ p [0 .. n-1]
-
-	 else do
-		parIO n mvs p
-		_ <- swapMVar busy False
-		return ()
+         else do
+                parIO gang action
+                _ <- swapMVar busy False
+                return ()
 
 
--- | Issue some requests to the worker threads and wait for them to complete.
-parIO 	:: Int			-- ^ Number of threads in the gang.
-	-> [MVar Req]		-- ^ Request vars for worker threads.
-	-> (Int -> IO ())	-- ^ Action to run in all the workers, it's given the ix of
-				--   the particular worker thread it's running on.
-	-> IO ()
+-- | Run an action on the gang sequentially.
+seqIO   :: Gang -> (Int -> IO ()) -> IO ()
+seqIO (Gang n _ _ _) action
+ = do   hPutStr stderr
+         $ unlines
+         [ "Data.Array.Repa: Performing nested parallel computation sequentially."
+         , "  You've probably called the 'compute' or 'copy' function while another"
+         , "  instance was already running. This can happen if the second version"
+         , "  was suspended due to lazy evaluation. Use 'deepSeqArray' to ensure"
+         , "  that each array is fully evaluated before you 'compute' the next one."
+         , "" ]
 
-parIO n mvs p
- = do	traceGang "parIO: begin"
+        mapM_ action [0 .. n-1]
 
-	start 	<- getGangTime
-	reqs	<- sequence . replicate n $ newReq p
+-- | Run an action on the gang in parallel.
+parIO   :: Gang -> (Int -> IO ()) -> IO ()
+parIO (Gang _ mvsRequest mvsResult _) action
+ = do	
+        -- Send requests to all the threads.
+        mapM_ (\v -> putMVar v (ReqDo action)) mvsRequest
 
-	traceGang "parIO: issuing requests"
-	_ <- zipWithM putMVar mvs reqs
-
-	traceGang "parIO: waiting for requests to complete"
-	mapM_ waitReq reqs
-	end 	<- getGangTime
-
-	traceGang $ "parIO: end " ++ diffTime start end
+        -- Wait for all the requests to complete.
+	mapM_ takeMVar mvsResult
 
 
 -- | Same as 'gangIO' but in the 'ST' monad.
 gangST :: Gang -> (Int -> ST s ()) -> ST s ()
 gangST g p = unsafeIOToST . gangIO g $ unsafeSTToIO . p
 
-
--- Tracing ----------------------------------------------------------------------------------------
-#if TRACE_GANG
-getGangTime :: IO Integer
-getGangTime
- = do	TOD sec pico <- getClockTime
-	return (pico + sec * 1000000000000)
-
-diffTime :: Integer -> Integer -> String
-diffTime x y = show (y-x)
-
-traceGang :: String -> IO ()
-traceGang s
- = do	t <- getGangTime
-	traceEvent $ show t ++ " @ " ++ s
-
-#else
-getGangTime :: IO ()
-getGangTime = return ()
-
-diffTime :: () -> () -> String
-diffTime _ _ = ""
-
-traceGang :: String -> IO ()
-traceGang _ = return ()
-
-#endif
 
 
