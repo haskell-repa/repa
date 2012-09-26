cafac2f9c1533a58acd099f1e6135ef2bd76067b
benl <benl@ouroborus.net>
2012-09-26 16:49:21 +1000

    repa-vector: add replicates to ChainChunk
    Ignore-this: 274ff11145a1c75d106c4491eab9a6ba
    
    darcs-hash:20120926064921-bf82d-0b4d741812015ed06ccb60036046151dcdef7b3c

--- repa-vector/fusion/ChainChunk.hs
+++ repa-vector/fusion/ChainChunk.hs
@@ -1,11 +1,16 @@
 {-# LANGUAGE BangPatterns, ExistentialQuantification, MagicHash, ScopedTypeVariables #-}
 
 module ChainChunk (test) where
-import qualified Data.Vector.Unboxed            as U
-import qualified Data.Vector.Unboxed.Mutable    as UM
+import Data.Array.Parallel.Unlifted.Parallel.UPSegd             (UPSegd)
+import qualified Data.Vector.Unboxed                            as U
+import qualified Data.Vector.Unboxed.Mutable                    as UM
+import qualified Data.Array.Parallel.Unlifted.Sequential.USegd  as USegd
+import qualified Data.Array.Parallel.Unlifted.Parallel.UPSegd   as UPSegd
+import qualified Data.Array.Parallel.Unlifted.Distributed.Primitive.DT  as D
 import Control.Monad.ST
 import System.IO.Unsafe
 import GHC.Exts
+
 
 data Step a s
         = Yield  a s
@@ -97,8 +102,8 @@ vunchain (Chain len chunks mkChunk)
 
         fillChunk write c (Chunk start end mkStep s0)
          = fillChunk' start s0
-
-         where  fillChunk' ix s
+         where  
+                fillChunk' ix s
                  | ix >=# end
                  = return ()
 
@@ -129,13 +134,75 @@ cmap f (Chain len chunks mkChunk)
                 {-# INLINE [0] mkStep' #-}
            in   Chunk start end mkStep' s0
         {-# INLINE [1] mkChunk' #-}
-
 {-# INLINE [1] cmap #-}
 
 
 -------------------------------------------------------------------------------
+vreplicates
+        :: U.Unbox a
+        => UPSegd
+        -> U.Vector a
+        -> Chain a
 
-test :: U.Vector Int -> U.Vector Int
-test vec
-        = vunchain $ cmap (+ 12345) $ vchain 4 vec
+vreplicates upsegd vec
+ = let  
+        -- Total number of elements to produce.
+        !(I# len)       = UPSegd.takeElements  upsegd
+
+        -- Get the distributed segment descriptor.
+        -- This tells us what elements each thread should work on.
+        !dists          = UPSegd.takeDistributed upsegd
+
+        -- Number of chunks in the distributed segment descriptor.
+        !(I# chunks)    = D.sizeD dists
+
+        -- Function to distribute work over gang,
+        -- 'c' is thread number
+        mkChunk !c
+         = let  -- TODO: Uh oh, this is assuming segd.takeDistributed works
+                --              the same as nstart...
+                --              We should use the offsets from takeDistributed instead.
+                !start  = nstart chunks len c
+                !end    = nstart chunks len (c +# 1#)
+        
+                -- Get current chunk of segment.
+                -- Segment offset describes where segment 0 in segd corrseponds in upsegd
+                ((segd, seg_off), _)    = D.indexD "replicates" dists (I# c)
+        
+                -- State: current segment -1 and remaining 0,
+                -- so first step will calculate real values
+                !state  = (segd, seg_off, -1, 0)
+
+           in   Chunk start end step state
+        {-# INLINE [2] mkChunk #-}
+
+
+        -- Stepper is called until the entire buffer is filled.
+        step _ (!segd, !seg_off, !seg_cur, !remain)
+         -- Go to next segment.
+         -- Don't need to handle end case because caller stops looping      
+         = if remain == 0
+            then
+             let !seg_cur'       = seg_cur + 1
+
+                 -- Find out how many copies we need to fill.
+                 (remain', _)    = USegd.getSeg segd seg_cur'
+
+                 -- Don't return a value, just update state .
+             in  Update (segd, seg_off, seg_cur', remain')
+
+            -- Return a value and decrement the count remaining   
+            else Yield  (vec `U.unsafeIndex` (seg_off + seg_cur))
+                        (segd, seg_off, seg_cur, remain - 1)
+        {-# INLINE [1] step #-}
+
+
+  in    Chain len chunks mkChunk
+{-# INLINE vreplicates #-}
+
+
+-------------------------------------------------------------------------------
+test :: UPSegd -> U.Vector Int -> U.Vector Int
+test !upsegd !vec
+        = vunchain $ cmap (+ 12345) $ vreplicates upsegd vec
 
