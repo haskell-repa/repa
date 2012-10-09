41a45c4695684d670915732910145f95db879c0b
benl <benl@ouroborus.net>
2012-10-09 17:37:12 +1100

    repa-vector: add Segd module
    Ignore-this: 78bf9c044ed78ceb5368431bee25115c
    
    darcs-hash:20121009063712-bf82d-d73ac1d294dde7b6677b7ca2bce1095d208a7f63

---
+++ repa-vector/Data/Array/Repa/Segmented/Segd.hs
@@ -0,0 +1,39 @@
+
+module Data.Array.Repa.Segmented.Segd
+        ( Segd (..)
+        , empty)
+where
+import Data.Array.Repa          as R
+import Data.Array.Repa.Vector   as R
+
+
+-- | Segment descriptor.
+data Segd r1 r2
+        = Segd
+        { -- | Length of each segment.
+          lengths       :: Vector r1 Int
+
+          -- | Starting index of each segment.
+        , indices       :: Vector r2 Int
+
+          -- | Number of elements in the flat array.
+        , elements      :: Int }
+
+
+-- | Construct an empty segment descriptor, 
+--   with no elements or segments.
+empty :: Segd D D
+empty   = Segd
+        { lengths       = R.fromFunction (Z :. 0) (const 0)
+        , indices       = R.fromFunction (Z :. 0) (const 0)
+        , elements      = 0 }
+
+
+-- | Construct a segment descriptor from a vector of lengths.
+-- fromLengths :: Vector r1 Int -> Segd r1 N
+-- fromLengths lens
+--        = Segd
+--        { lengths       = lens
+--        , indices       = vscanl (+) 0 lens
+--        , elements      = vlength lens }
+
