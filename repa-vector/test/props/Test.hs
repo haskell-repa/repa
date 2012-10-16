45f98c1816ed2bc7e458a8f16a3a7c2d507ccc92
Ben Lippmeier <benl@ouroborus.net>
2012-10-16 22:02:24 +1100

    repa-vector: add property for replicates
    Ignore-this: a2b331224e1f2e5d45d6ec78f7c8dc39
    
    darcs-hash:20121016110224-08f32-49d2e233c15d2c10f3014cf7669370a01daecf92

--- repa-vector/test/props/Test.hs
+++ repa-vector/test/props/Test.hs
@@ -1,14 +1,18 @@
 {-# LANGUAGE TypeOperators, FlexibleContexts, FlexibleInstances, TypeSynonymInstances #-}
-{-# LANGUAGE ParallelListComp #-}
+{-# LANGUAGE ParallelListComp, MagicHash, BangPatterns #-}
 
 import Test.Framework
 import Test.Framework.Providers.QuickCheck2
-import Test.QuickCheck
+import Test.QuickCheck                  
 import Data.Array.Repa
 import Data.Array.Repa.Vector
+import Data.Array.Repa.Vector.Segd              (Segd(Segd))
 import System.IO.Unsafe
-import qualified Data.Vector.Unboxed    as U
-import Prelude                          as P
+import qualified Data.Vector.Unboxed            as U
+import qualified Data.Array.Repa.Vector.Segd    as Segd
+import Prelude                                  as P
+import Data.List
+import GHC.Exts 
 
 
 instance Num Z where
@@ -40,6 +44,30 @@ instance (Arbitrary a, U.Unbox a)
   = do  elems           <- arbitrary
         return $ fromListUnboxed (Z :. length elems) elems
 
+
+-- | Segment descriptor of length n.
+-- 
+--   Do not use directly unless an arbitrary Segd is all you need.  
+--   Use segdForArray to generate a Segd that fits the array.
+-- 
+instance Arbitrary (Segd U U) where
+ arbitrary 
+  = sized $ \n@(I# n') 
+  -> do ids             <- genIndices n
+        let lens        = indicesToLengths ids n
+        return $ Segd (vfromListUnboxed lens) (vfromListUnboxed ids) n'
+  where 
+        -- list of non-decreasing integers in range [0, n)
+        genIndices 0 = return []
+        genIndices n = ((0:) . sort . P.map (`mod` n)) `fmap` arbitrary
+        indicesToLengths ids n = P.zipWith (-) (tail $ ids P.++ [n]) ids
+
+
+-- | Generate a segment descriptor fitting the given vector
+segdForVector :: Source r a => Vector r a -> Gen (Segd U U)
+segdForVector arr = resize (vlength arr) arbitrary
+
+
 -- Framework ------------------------------------------------------------------
 main = defaultMain tests
 
@@ -48,6 +76,7 @@ tests
         , testProperty "compute/stream"         prop_compute_stream
         , testProperty "compute/toStream/chain" prop_compute_toStream_chain
         , testProperty "replicate"              prop_replicate
+        , testProperty "replicates"             prop_replicates
         , testProperty "indexed/U"              prop_indexedU 
         , testProperty "indexed/N"              prop_indexedN
         , testProperty "pack"                   prop_pack
@@ -77,8 +106,23 @@ prop_compute_toStream_chain vec
 prop_replicate :: Positive Int -> Int -> Bool
 prop_replicate (Positive len) val
  = let len'     = min len 10000
-   in  replicate len' val
-    == vtoList  (vreplicate len' val)
+   in  vtoList  (vreplicate len' val)
+    == replicate len' val
+
+
+prop_replicates :: [Int] -> [Int] -> Bool
+prop_replicates lens0 vec0
+ = let  (lens, vec)     = P.unzip $ P.zip (P.map (`mod` maxRepl) lens0) vec0
+        maxRepl         = 100
+
+        lens'           = vfromListUnboxed lens
+        idxs'           = vfromListUnboxed (scanl (+) 0 lens)
+        vec'            = vfromListUnboxed vec
+        !(I# n')        = P.sum lens
+        segd            = Segd lens' idxs' n'
+
+   in   toList (vreplicates segd vec')
+    ==  P.concat (P.zipWith P.replicate lens vec)
 
 
 -- Indexed --------------------------------------------------------------------
