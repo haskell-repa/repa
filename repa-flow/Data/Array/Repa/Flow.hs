4cdd6fc15cd832a6b0ab511be420acad6cc034be
Ben Lippmeier <benl@ouroborus.net>
2012-10-26 15:10:44 +1100

    repa-flow: add more ops
    Ignore-this: 477f9023a96ef2fec9d2775d80f8e327
    
    darcs-hash:20121026041044-08f32-0c1081e5de684d4c3fbe2329b2be27a66ce04e03

--- repa-flow/Data/Array/Repa/Flow.hs
+++ repa-flow/Data/Array/Repa/Flow.hs
@@ -1,15 +1,21 @@
-{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE MagicHash, BangPatterns #-}
 module Data.Array.Repa.Flow
         ( Flow (..)
         , flow
         , unflow
-        , map)
+        , map
+        , zip
+        , zipWith
+        , foldl
+        , indexs
+        , pack
+        , filter)
 where
 import Data.IORef
 import GHC.Exts
 import qualified Data.Vector.Unboxed            as U
 import qualified Data.Vector.Unboxed.Mutable    as UM
-import Prelude  hiding (map)
+import Prelude  hiding (map, zip, zipWith, foldl, filter)
 
 
 --  With perfect stream fusion we'll end up with a single loop that
@@ -42,8 +48,15 @@ data Flow a
 
 data Size
         = Exact Int
+        | Max   Int
 
-
+sizeMin :: Size -> Size -> Size
+sizeMin s1 s2
+ = case (s1, s2) of
+        (Exact len1, Exact len2)        -> Exact (min len1 len2)
+        (Exact len1, Max   len2)        -> Max   (min len1 len2)
+        (Max   len1, Exact len2)        -> Max   (min len1 len2)
+{-# INLINE [0] sizeMin #-}
 
 
 -- map ------------------------------------------------------------------------
@@ -68,6 +81,130 @@ map f (Flow size pull1 pull4)
 
                  Right len
                   -> return $ Right len
+{-# INLINE [1] map #-}
+
+
+-- zip ------------------------------------------------------------------------
+zip :: Flow a -> Flow b -> Flow (a, b)
+zip     (Flow sizeA pullA1 pullA4)
+        (Flow sizeB pullB1 pullB4)
+ = Flow (sizeMin sizeA sizeB) pull1' pull4'
+ where
+        pull1' ()
+         = do   !mxA     <- pullA1 ()
+                !mxB     <- pullB1 ()
+                case (mxA, mxB) of
+                 (Just xA, Just xB)
+                   -> return $ Just $ (xA, xB)
+
+                 _ -> return $ Nothing
+
+        pull4' ()
+         = do   !mxA     <- pullA4 ()
+                !mxB     <- pullB4 ()
+                case (mxA, mxB) of
+                 (  Left (a1, a2, a3, a4)
+                  , Left (b1, b2, b3, b4))
+                  -> return $ Left  ((a1, b1), (a2, b2), (a3, b3), (a4, b4))
+
+                 (Right len, _) 
+                  -> return $ Right len
+
+                 (_, Right len) 
+                  -> return $ Right len
+{-# INLINE [1] zip #-}
+
+
+-- zipWith --------------------------------------------------------------------
+zipWith :: (a -> b -> c) -> Flow a -> Flow b -> Flow c
+zipWith f flowA flowB
+        = map (uncurry f) $ zip flowA flowB
+{-# INLINE [1] zipWith #-}
+
+
+-- foldl ----------------------------------------------------------------------
+foldl :: (a -> b -> a) -> a -> Flow b -> IO a
+foldl f z (Flow size pull1 pull4)
+ = eat4 z
+ where
+        eat1 !acc
+         = do   !mx     <- pull1 ()
+                case mx of
+                 Just x1
+                  -> eat1 (acc `f` x1)
+
+                 Nothing
+                  -> return acc
+
+        eat4 !acc 
+         = do   !mx     <- pull4 ()
+                case mx of
+                 Left (x1, x2, x3, x4)
+                  -> eat4 (acc `f` x1 `f` x2 `f` x3 `f` x4)
+
+                 Right _
+                  -> eat1 acc
+{-# INLINE [1] foldl #-}
+
+
+-- indexs ---------------------------------------------------------------------
+indexs :: U.Unbox a => U.Vector a -> Flow Int -> Flow a
+indexs vec (Flow size pull1 pull4)
+ = Flow size pull1' pull4'
+ where
+        pull1' ()
+         = do   !mix    <- pull1 ()
+                case mix of
+                 Just ix        
+                  -> return $ Just (U.unsafeIndex vec ix)
+
+                 Nothing        
+                  -> return $ Nothing
+
+        pull4' ()
+         = do   !mix    <- pull4 ()
+                case mix of
+                 Left (ix1, ix2, ix3, ix4)
+                  -> return $ Left ( U.unsafeIndex vec ix1
+                                   , U.unsafeIndex vec ix2
+                                   , U.unsafeIndex vec ix3
+                                   , U.unsafeIndex vec ix4)
+
+                 Right len
+                  -> return $ Right len
+{-# INLINE [1] indexs #-}
+
+
+-- pack -----------------------------------------------------------------------
+pack :: U.Unbox a => Flow (Bool, a) -> IO (Flow a)
+pack (Flow size pull1 pull4)
+ = newIORef Stash0 >>= \refStash
+ -> let 
+        size'
+         = case size of
+                Exact len       -> Max len
+                Max   len       -> Max len
+
+        pull1' ()
+         = do   !mx   <- pull1 ()
+                case mx of
+                 Just (True,  x) -> return $ Just x
+                 Just (False, _) -> pull1' ()
+                 Nothing         -> return $ Nothing
+
+        pull4' ()
+         = do   return $ Right 1
+
+    in  return $ Flow size pull1' pull4'
+
+{-# INLINE [1] pack #-}
+
+
+-- filter ---------------------------------------------------------------------
+filter :: U.Unbox a => (a -> Bool) -> Flow a -> IO (Flow a)
+filter f ff
+        = pack $ map (\x -> (f x, x)) ff
+{-# INLINE [1] filter #-}
 
 
 -- flow -----------------------------------------------------------------------
@@ -109,6 +246,7 @@ unflow :: U.Unbox a => Flow a -> IO (U.Vector a)
 unflow flow
  = case flowSize flow of
         Exact len       -> unflowExact flow len
+        Max   len       -> unflowExact flow len         -- wrong
 {-# INLINE [1] unflow #-}
 
 
