09176d4597cafaafd0d0808c85bbc295910ed45f
Ben Lippmeier <benl@ouroborus.net>
2012-11-06 10:31:43 +1100

    repa-flow: add missing Par module
    Ignore-this: f6139833f71ba5e2fc6576d19d34cacf
    
    darcs-hash:20121105233143-08f32-69a04e8482d5b18d10c87e71c0071875e261528c

---
+++ repa-flow/Data/Array/Repa/Flow/Par.hs
@@ -0,0 +1,12 @@
+
+module Data.Array.Repa.Flow.Par
+        ( Flow   (..)
+        , Distro (..)
+        , flow
+        , Unflow (..)
+        , map)
+where
+import Data.Array.Repa.Flow.Par.Base
+import Data.Array.Repa.Flow.Par.Map
+import Prelude hiding (map)
+
