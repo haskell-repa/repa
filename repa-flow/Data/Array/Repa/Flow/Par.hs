0c8e12403251e133759521dfa73a5ce035e24acf
Ben Lippmeier <benl@ouroborus.net>
2012-11-06 20:17:20 +1100

    repa-flow: add parallel gather
    Ignore-this: 62de764c4a07b02e382457710395773e
    
    darcs-hash:20121106091720-08f32-6d36ec92245810de2dede48ab7f2802c0fd1e838

--- repa-flow/Data/Array/Repa/Flow/Par.hs
+++ repa-flow/Data/Array/Repa/Flow/Par.hs
@@ -24,11 +24,15 @@ module Data.Array.Repa.Flow.Par
         -- * Packing
         , packByTag
         , pack
-        , filter)
+        , filter
+
+        -- * Projection
+        , gather)
 where
 import Data.Array.Repa.Flow.Par.Base
 import Data.Array.Repa.Flow.Par.Map
 import Data.Array.Repa.Flow.Par.Generate
 import Data.Array.Repa.Flow.Par.Pack
+import Data.Array.Repa.Flow.Par.Project
 import Prelude hiding (map, zip, zipWith, replicate, filter)
 
