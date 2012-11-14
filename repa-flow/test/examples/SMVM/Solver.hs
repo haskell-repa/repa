b0dec6529786cbd6b9ee0321b9329ec420b769e0
Ben Lippmeier <benl@ouroborus.net>
2012-11-14 15:31:25 +1100

    repa-flow: fix SMVM solver
    Ignore-this: b1d347ad3ac268986c991996415a0f1d
    
    darcs-hash:20121114043125-08f32-e12b3d67959d3e5f92d40db37c19db7508d7c889

--- repa-flow/test/examples/SMVM/Solver.hs
+++ repa-flow/test/examples/SMVM/Solver.hs
@@ -3,9 +3,9 @@ module Solver
         (smvm)
 where
 import Data.Vector.Unboxed
-import Data.Array.Repa.Flow             (Flow)
-import qualified Data.Array.Repa.Flow   as F
-import qualified Data.Vector.Unboxed    as U
+import Data.Array.Repa.Flow.Seq                 (Flow)
+import qualified Data.Array.Repa.Flow.Seq       as F
+import qualified Data.Vector.Unboxed            as U
 
 
 -- {-# NOINLINE smvm #-}
@@ -25,17 +25,17 @@ import qualified Data.Vector.Unboxed    as U
 smvm    :: Vector Int           -- ^ Row lengths for matrix.
         -> Vector (Int, Double) -- ^ Sparse matrix column number and coefficient.
         -> Vector Double        -- ^ Dense vector.
-        -> IO (Vector Double)
+        -> Vector Double
 
 smvm !vLens !vMatrix !vVector
- = do   fLens           <- F.flow vLens
+ = let  fLens   = F.flow vLens
 
-        let (vColId, vColVal)   = U.unzip vMatrix
-        fColId          <- F.flow vColId
-        fColVal         <- F.flow vColVal
+        (vColId, vColVal)   = U.unzip vMatrix
+        fColId  = F.flow vColId
+        fColVal = F.flow vColVal
 
-        let fVals       = smvm' fLens fColId fColVal vVector
-        F.unflow fVals
+        fVals   = smvm' fLens fColId fColVal vVector
+   in   F.unflow fVals
 
 smvm' fLens fColId fColVal !vVector
  = let  fCoeffs     = F.gather vVector fColId 
