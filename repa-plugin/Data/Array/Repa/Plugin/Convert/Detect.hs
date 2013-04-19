77cbcecfcea6264b0a0734b9d6740f828cc923da
Ben Lippmeier <benl@ouroborus.net>
2013-04-19 20:44:07 +1000

    repa-plugin: detect rate variables and set the proper kind
    Ignore-this: 7e0f24e3a032b007fc95cb2deccb92f6
    
    darcs-hash:20130419104407-08f32-1beca8f87335807435410d9af477812de32bcb28

--- repa-plugin/Data/Array/Repa/Plugin/Convert/Detect.hs
+++ repa-plugin/Data/Array/Repa/Plugin/Convert/Detect.hs
@@ -11,28 +11,62 @@ import DDC.Core.Flow.Compounds
 import DDC.Core.Flow.Env
 import Data.Array.Repa.Plugin.Convert.FatName
 import Data.List
-import qualified DDC.Type.Sum   as Sum
-import qualified Data.Map       as Map
 import Data.Map                 (Map)
+import Data.Set                 (Set)
 import Control.Monad.State.Strict
 
-
-type DetectS     
-        = Map Name GhcName
+import qualified DDC.Type.Sum   as Sum
+import qualified Data.Map       as Map
+import qualified Data.Set       as Set
 
 
 detectModule :: Module a FatName -> Module a Name
 detectModule mm
-        = evalState (detect mm) $ Map.empty
+        = evalState (detect mm) $ zeroState
 
 
--- | Detect flow operators in code converted from GHC Core.
+-- | Detect flow operators in code converted from GHC Core, rewriting the raw
+--   AST converted from GHC to be a well formed Disciple Core program. 
+--   After this pass the code should type check.
+--
 class Detect (c :: * -> *) where
  detect :: c FatName -> State DetectS (c Name)
 
+
+-- Detect State ---------------------------------------------------------------
+data DetectS     
+        = DetectS
+        { -- Map of Disciple Core names to GHC Core Names.
+          stateNames    :: Map Name GhcName
+
+          -- Names of rate variables, which we discover as they are arguments
+          -- of Stream type constructors. 
+          -- In GHC core rate variables have kind '*', 
+          --   but for Disciple Core we change them to have kind 'Rate'.
+        , stateRateVars :: Set Name }
+
+
+zeroState :: DetectS
+zeroState
+        = DetectS
+        { stateNames    = Map.empty
+        , stateRateVars = Set.empty }
+
+
 collect :: Name -> GhcName -> State DetectS ()
-collect d g
- = modify $ Map.insert d g
+collect !d !g
+ = modify $ \s -> s { stateNames    = Map.insert d g (stateNames s) }
+
+
+setRateVar :: Name -> State DetectS ()
+setRateVar !name
+ = modify $ \s -> s { stateRateVars = Set.insert name (stateRateVars s) }
+
+
+isRateVar  :: Name -> State DetectS Bool
+isRateVar name
+ = do   s       <- gets stateRateVars 
+        return  $ Set.member name s
 
 
 -- Bind -----------------------------------------------------------------------
@@ -122,12 +156,37 @@ instance Detect TyCon where
 -- Type ------------------------------------------------------------------------
 instance Detect Type where
  detect tt
+
+  -- Detect rate variables being applied to Stream type constructors.
+  | TApp t1 t2  <- tt
+  , [ TCon (TyConBound (UName (FatName _ (NameCon str))) _)
+    , TVar             (UName (FatName _ n))
+    , _]  
+                <- takeTApps tt
+  , isPrefixOf "Stream_" str
+  = do  setRateVar n
+        t1'     <- detect t1
+        t2'     <- detect t2
+        return  $ TApp t1' t2'
+
+  -- Set kind of detected rate variables to Rate.
+  | TForall b t <- tt
+  = do  t'      <- detect t
+        b'      <- detect b
+        case b' of
+         BName n _
+          -> do rateVar <- isRateVar n
+                if rateVar
+                 then return $ TForall (BName n kRate) t'
+                 else return $ TForall b' t'
+
+  -- Boilerplate traversal.
+  | otherwise
   = case tt of
         TVar u          -> liftM  TVar    (detect u)
         TCon c          -> liftM  TCon    (detect c)
         TForall b t     -> liftM2 TForall (detect b) (detect t)
         TApp t1 t2      -> liftM2 TApp    (detect t1) (detect t2)
-
         TSum ts         
          -> do  k       <- detect $ Sum.kindOfSum ts
                 tss'    <- liftM (Sum.fromList k) $ mapM detect $ Sum.toList ts
@@ -164,6 +223,18 @@ matchPrimArith str
 instance Detect (Exp a) where
  detect xx
 
+  -- Set kind of detected rate variables to Rate.
+  | XLam a b x          <- xx
+  = do  b'      <- detect b
+        x'      <- detect x
+        case b' of
+         BName n _
+          -> do rateVar <- isRateVar n
+                if rateVar 
+                 then return $ XLAM a (BName n kRate) x'
+                 else return $ XLam a b' x'
+
+
   -- Detect folds.
   | XApp a _ _                            <- xx
   , Just (XVar _ uFold, [xTK, xTA, xTB, xF, xZ, xS])
@@ -189,13 +260,13 @@ instance Detect (Exp a) where
   -- Boilerplate traversal.
   | otherwise
   = case xx of
-        XVar  a u       -> liftM2 XVar     (return a) (detect u)
-        XCon  a u       -> liftM2 XCon     (return a) (detect u)
-        XLAM  a b x     -> liftM3 XLAM     (return a) (detect b)   (detect x)
-        XLam  a b x     -> liftM3 XLam     (return a) (detect b)   (detect x)
-        XApp  a x1 x2   -> liftM3 XApp     (return a) (detect x1)  (detect x2)
-        XLet  a lts x   -> liftM3 XLet     (return a) (detect lts) (detect x)
-        XType t         -> liftM  XType    (detect t)
+        XVar  a u       -> liftM2 XVar  (return a) (detect u)
+        XCon  a u       -> liftM2 XCon  (return a) (detect u)
+        XLAM  a b x     -> liftM3 XLAM  (return a) (detect b)   (detect x)
+        XLam  a b x     -> liftM3 XLam  (return a) (detect b)   (detect x)
+        XApp  a x1 x2   -> liftM3 XApp  (return a) (detect x1)  (detect x2)
+        XLet  a lts x   -> liftM3 XLet  (return a) (detect lts) (detect x)
+        XType t         -> liftM  XType (detect t)
 
         XCase a x alts  -> error "repa-plugin.detect: XCase not handled"
         XCast a c x     -> error "repa-plugin.detect: XCast not handled"
