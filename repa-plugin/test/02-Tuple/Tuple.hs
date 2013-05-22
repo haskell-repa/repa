3ebe66a32418676a04459eacf440c8ba0c154be4
Amos Robinson <amos.robinson@gmail.com>
2013-05-22 16:43:42 +1000

    repa-plugin: test for returning a pair
    Ignore-this: 3cb3be759b7eb8eeec87b8dab05824b3
    
    darcs-hash:20130522064342-209a3-8dd9b90e3cd0dca81576d27b009da04ce3ab6d09

---
+++ repa-plugin/test/02-Tuple/Tuple.hs
@@ -0,0 +1,34 @@
+module Main where
+import Data.Array.Repa.Series           as R
+import Data.Array.Repa.Series.Series    as S
+import Data.Array.Repa.Series.Vector    as V
+import qualified Data.Vector.Unboxed    as U
+
+---------------------------------------------------------------------
+-- | Set the primitives used by the lowering transform.
+repa_primitives :: R.Primitives
+repa_primitives =  R.primitives
+
+
+---------------------------------------------------------------------
+main
+ = do   v1      <- V.fromUnboxed $ U.enumFromN (1 :: Int) 10
+        print $ R.runSeries v1 lower_fffold
+--        print $ R.runSeries v1 lower_fold_map
+
+
+-- Triple fold fusion.
+--  We end up with an extra let-binding for the second baseband 
+--  addition that needs to be handled properly.
+lower_fffold :: R.Series k Int -> (Int, Int)
+lower_fffold s
+ = (R.fold (+) 0 s + R.fold (*) 1 s, R.fold (*) 1 s)
+
+{-
+-- Fold a series while mapping across it.
+--  The source elements are only read from memory once.
+lower_fold_map :: R.Series k Int -> (Int, Vector Int)
+lower_fold_map s
+ = ( R.fold (+) 0 s
+   , S.toVector (R.map  (\x -> x * 2) s))
+ -}
