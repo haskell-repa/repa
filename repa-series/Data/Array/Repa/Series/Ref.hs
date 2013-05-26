2fe89e8d3752a3d91a7b3517328a60110446263e
Ben Lippmeier <benl@ouroborus.net>
2013-05-26 13:44:15 +1000

    repa-series: add Ref module
    Ignore-this: b1361461aeea4b044464dabb8ac3d54b
    
    darcs-hash:20130526034415-08f32-7bab913f34e656a33926b1c7e54969b7390c5dfd

---
+++ repa-series/Data/Array/Repa/Series/Ref.hs
@@ -0,0 +1,38 @@
+
+module Data.Array.Repa.Series.Ref
+        ( Ref (..)
+        , new
+        , read
+        , write)
+where
+import Data.Vector.Unboxed.Mutable              (IOVector, Unbox)
+import qualified Data.Vector.Unboxed.Mutable    as UM
+import Prelude hiding (read)
+
+
+data Ref a
+        = Ref !(IOVector a)
+
+
+-- | Create a new unboxed reference.
+new :: Unbox a => a -> IO (Ref a)
+new x
+ = do   vec     <- UM.new 1
+        UM.unsafeWrite vec 0 x
+        return  (Ref vec)
+{-# INLINE new #-}
+
+
+-- | Read from an unboxed reference.
+read :: Unbox a => Ref a -> IO a
+read (Ref vec)
+ =      UM.unsafeRead vec 0
+{-# INLINE read #-}
+
+
+-- | Write to an unboxed reference.
+write :: Unbox a => Ref a -> a -> IO ()
+write (Ref vec) x
+ = do   UM.unsafeWrite vec 0 x
+{-# INLINE write #-}
+
