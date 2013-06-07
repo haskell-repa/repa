d0de7b3d4d4d9b28a228faef08518b61780bc220
Amos Robinson <amos.robinson@gmail.com>
2013-06-07 17:58:34 +1000

    repa-plugin: quickhull example. compiles, doesn't really work. ugly
    Ignore-this: afb91bcc0cf47f1f823a07aaa84a4f8d
    
    darcs-hash:20130607075834-209a3-617decf020a610c4874f2d593c35f6aea2f66358

--- repa-plugin/test/07-SimpleBench/Bench_QuickHull.hs
+++ repa-plugin/test/07-SimpleBench/Bench_QuickHull.hs
@@ -30,52 +30,58 @@ quickhull ps
  | U.length ps == 0
  = return U.empty
  | otherwise
- = do   v <- V.fromUnboxed ps
-        xs <- V.fromUnboxed (U.map fst ps)
-        ys <- V.fromUnboxed (U.map snd ps)
+ = do   let (uxs, uys) = U.unzip ps
+        xs <- V.fromUnboxed uxs
+        ys <- V.fromUnboxed uys
         let (ix,iy)     = ps U.! 0
-            Just (imin,imax) = R.runSeries2 xs ys (lower_minmax ix iy)
+            Just ((_,imin),(_,imax)) = R.runSeries2 xs ys (lower_minmax ix iy)
             pmin        = ps U.! imin
             pmax        = ps U.! imax
 
-        u1 <- hsplit v pmin pmax
-        u2 <- hsplit v pmax pmin
-        return (u1 U.++ u2)
+        u1 <- hsplit xs ys pmin pmax
+        u2 <- hsplit xs ys pmax pmin
+        return (uncurry U.zip u1 U.++ uncurry U.zip u2)
  where
-  hsplit v p1@(x1,y1) p2@(x2,y2)
-   = do  let (packed,pm) = R.runSeries v (zlower_filtermax x1 y1 x2 y2)
-         case V.length packed <# 2# of
+  hsplit xs ys p1@(x1,y1) p2@(x2,y2)
+   = do  let Just (pxs,pys,(_,im)) = R.runSeries2 xs ys (lower_filtermax x1 y1 x2 y2)
+         upxs <- V.toUnboxed pxs
+         upys <- V.toUnboxed pys
+         case V.length pxs <# 2# of
           True
-           -> do upacked <- V.toUnboxed packed
-                 return ((x1,x2) `U.cons` upacked)
+           ->    return (x1 `U.cons` upxs, y1 `U.cons` upys)
           False
-           -> do u1 <- hsplit packed p1 pm
-                 u2 <- hsplit packed pm p2
-                 return (u1 U.++ u2)
+           -> do let pm = (upxs U.! im, upys U.! im)
+                 (ux1,uy1) <- hsplit pxs pys p1 pm
+                 (ux2,uy2) <- hsplit pxs pys pm p2
+                 return (ux1 U.++ ux2, uy1 U.++ uy2)
 
 
 lower_minmax :: Int -> Int
              -> R.Series k Int
              -> R.Series k Int
-             -> (Int, Int)
+             -> ((Int,Int), (Int,Int))
 lower_minmax ix iy xs ys
- = let (_,imin) = R.foldIndex (\i (x',i') x -> if x < x' then (x, I# i) else (x',i')) (ix,0) xs
-       (_,imax) = R.foldIndex (\i (x',i') x -> if x > x' then (x, I# i) else (x',i')) (ix,0) xs
+ = let imin = R.foldIndex (\i (x',i') x -> if x < x' then (x, I# i) else (x',i')) (ix,0) xs
+       imax = R.foldIndex (\i (x',i') x -> if x > x' then (x, I# i) else (x',i')) (ix,0) xs
    in  (imin,imax)
 
 
-zlower_filtermax
+lower_filtermax
              :: Int -> Int
              -> Int -> Int
-             -> R.Series k (Int, Int)
-             -> (R.Vector (Int,Int), (Int,Int))
-zlower_filtermax x1 y1 x2 y2 vs
- = let cs    = R.map    (\(x,y) -> ((x,y), (x1-x)*(y2-y) - (y1-y)*(x2-x))) vs
-       pmax  = R.fold   (\((x,y),d) ((x',y'),d') -> if d > d' then ((x,y),d) else ((x',y'),d')) ((0,0),0) cs
-       pack  = R.map    (\((x,y),d) -> d > 0) cs
+             -> R.Series k Int
+             -> R.Series k Int
+             -> (R.Vector Int, R.Vector Int, (Int,Int))
+lower_filtermax x1 y1 x2 y2 xs ys
+ = let cs    = R.map2   (\x y -> (x1-x)*(y2-y) - (y1-y)*(x2-x)) xs ys
+--       pmax  = R.fold   (\((x,y),d) ((x',y'),d') -> if d > d' then ((x,y),d) else ((x',y'),d')) ((0,0),0) cs
+       pack  = R.map    (\d -> d > 0) cs
    in R.mkSel1 pack (\sel ->
-       let ps'   = R.pack sel vs
-           (pm,_)= pmax
-       in  (S.toVector ps', pm))
+       let xs'   = R.pack sel xs
+           ys'   = R.pack sel ys
+           cs'   = R.pack sel cs
+           pmax  = R.foldIndex (\i (c',i') c -> if c > c' then (c, I# i) else (c', i')) (0,0) cs'
+           -- (pm,_)= pmax
+       in  (S.toVector xs', S.toVector ys', pmax))
 
 
