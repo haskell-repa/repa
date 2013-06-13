10504bba356c6e0cdc93b78456cf9a8a839b6fc4
Ben Lippmeier <benl@ouroborus.net>
2013-06-13 13:09:31 +1000

    Benchmarks: makefile wibbles, use -O3
    Ignore-this: 5e0f73ae5956d61364e64f523c1afd14
    
    darcs-hash:20130613030931-08f32-686ff62b26a06611c50c780a3038eed2949356f9

--- repa-plugin/test/07-SimpleBench/c/src/Main_FilterMax.c
+++ repa-plugin/test/07-SimpleBench/c/src/Main_FilterMax.c
@@ -6,49 +6,50 @@
 #include "Timing.h"
 
 
-void filtermax(long lens, long* uvec, long* out, long* out_len, long* out_max, long* out_min)
+void filtermax(long size, long* uvec, long* out, long* out_len, long* out_max)
 {
-	// ASSERT lens /= 0 ?
-	long len = 0;
-	long max = uvec[0]+1;
-	long min = uvec[0]+1;
-	for (long i = 0; i != lens; ++i) {
+        assert (size > 0);
+        
+	long len       = 0;
+	long max       = uvec[0] + 1;
+
+	for (long i = 0; i < size; i++) {
 		long s2 = uvec[i] + 1;
 		if (s2 > 0) {
 			out[len++] = s2;
 			if (s2 > max) {
 				max = s2;
 			}
-			if (s2 < min) {
-				min = s2;
-			}
 		}
 	}
 	*out_len = len;
 	*out_max = max;
-	*out_min = min;
 }
+
 
 int main(int argc, char** argv)
 {
 	// Parse cmd line args.
-	int	pointCount	= 0;
+	int	size	      = 0;
 
 	if(argc == 2) {
-		pointCount	= atoi(argv[1]);
+		size          = atoi(argv[1]);
 	}
 	else {
-		printf("usage: quickhull <points>\n");
+		printf("usage: filtermax <size>\n");
 		exit(1);
 	}
 
-	long* uvec		= malloc(pointCount * sizeof(long));
+        // Input vector.
+	long* uvec     = malloc(size * sizeof(long));
 
-	long* out		= malloc(pointCount * sizeof(long));
+        // 
+        for (int i = 0; i < size; i++) {
+                uvec[i] = i;
+        }
 
-	for (int i = 0; i < pointCount; i++) {
-		uvec[i] = i;
-	}
+        // Output vector.
+	long* out      = malloc(size * sizeof(long));
 
 	// Timing setup
         struct timeval start, finish;
@@ -58,15 +59,12 @@ int main(int argc, char** argv)
         getrusage( RUSAGE_SELF, &start_ru );
 
 	// Do the deed.
-	long out_len, out_max, out_min;
-	filtermax(pointCount, uvec, out, &out_len, &out_max, &out_min);
+	long out_len, out_max;
+	filtermax(size, uvec, out, &out_len, &out_max);
 
 	// Print how long it took.
         gettimeofday( &finish, NULL );
         getrusage( RUSAGE_SELF, &finish_ru );
-
-//	printf("depth          = %d\n", depth);
-//	printf("points on hull = %d\n", hull->length);
 
         sub_timeval( &finish, &start );
         sub_timeval( &finish_ru.ru_utime, &start_ru.ru_utime );
