015b0f4369c5a3b6de1a7d00804935eec96f79cc
Ben Lippmeier <benl@ouroborus.net>
2013-11-02 15:30:55 +1100

    plugin: fix up points example
    Ignore-this: 8035370e781886d2dda9d26ec8e35a36
    
    darcs-hash:20131102043055-08f32-40b325e09e6049d01be1d11b0108af6e2ed0afb7

--- repa-plugin/test/90-Benchmarks/repa/Points/Main.hs
+++ repa-plugin/test/90-Benchmarks/repa/Points/Main.hs
@@ -3,7 +3,10 @@ module Main where
 import Data.Array.Repa.Series           as R
 import Data.Array.Repa.Series.Series    as S
 import Data.Array.Repa.Series.Vector    as V
-import qualified Data.Vector.Unboxed    as U
+import qualified Data.Vector.Primitive  as P
+import Data.Array.Repa.Series.Ref       as Ref
+import Data.Array.Repa.Series.Ref       (Ref)
+import Data.Array.Repa.IO.Timing
 
 import GHC.Exts
 import System.Environment
@@ -19,20 +22,33 @@ main
         let sz = case args of
                    [szStr] -> (Prelude.read szStr :: Int)
                    _       -> error "Usage: simplebench <size>"
-        v1      <- V.fromUnboxed $ U.enumFromN (0 :: Int) sz `U.zip` U.enumFromN (0 :: Int) sz
-        let (ys,zs) = R.runSeries v1 (lower_points 5 5)
-        print (ys,zs)
+
+        v0      <- (V.new sz :: IO (V.Vector Float))
+        v1      <- V.fromPrimitive $ P.enumFromN 0 sz
+        v2      <- V.fromPrimitive $ P.enumFromN 0 sz
+        v0 `seq` v1 `seq` v2 `seq` return ()
+
+        r0      <- Ref.new 0
+        ((p0, x), t) 
+                <- time
+                $ do    True    <- runProcess2 v1 v2 (eatPoints 5 5 v0 r0)
+                        p0      <- V.toPrimitive v0
+                        x       <- Ref.read r0
+                        p0 `seq` x `seq` return (p0, x)
+        putStr $ prettyTime t
+        print x
 
 
-lower_points :: Int -> Int
-             -> R.Series k (Int, Int)
-             -> (R.Vector Int, Int)
+eatPoints
+        :: Float -> Float
+        -> R.Vector Float   -> Ref Float
+        -> RateNat k
+        -> R.Series k Float -> R.Series k Float
+        -> Process
 
-lower_points ox oy xs
- = let dists = R.map  (\(x,y) -> (x-ox)*(x-ox) + (y-oy)*(y-oy)) xs
-       mini  = R.fold min 1000 dists
-   in  (S.toVector dists, mini)
-
-
-
-
+eatPoints ox oy v0 r0 _ s1 s2
+ = let  s       = R.map2 (\x y -> (x-ox) * (x-ox) + (y-oy)* (y-oy))
+                         s1 s2
+   in   R.pjoin (R.reduce r0 (+) 0 s1)
+                (R.fill   v0 s)
+{-# NOINLINE eatPoints #-}
