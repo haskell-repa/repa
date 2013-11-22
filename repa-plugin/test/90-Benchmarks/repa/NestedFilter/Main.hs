9be354392e8beee9f12473cf0866c6ca1ef1215c
Ben Lippmeier <benl@ouroborus.net>
2013-11-22 17:44:44 +1100

    update NestedFilter to new api
    Ignore-this: 394e07c469192cac9b159dcd5d655de1
    
    darcs-hash:20131122064444-08f32-022af30ee4f2f631d8fc2f8d240a4fabcc646378

--- repa-plugin/test/90-Benchmarks/repa/NestedFilter/Main.hs
+++ repa-plugin/test/90-Benchmarks/repa/NestedFilter/Main.hs
@@ -3,7 +3,7 @@ module Main (main, repa_primitives) where
 import Data.Array.Repa.Series           as R
 import Data.Array.Repa.Series.Series    as S
 import Data.Array.Repa.Series.Vector    as V
-import qualified Data.Vector.Unboxed    as U
+import qualified Data.Vector.Primitive  as P
 import Data.Array.Repa.IO.Timing
 
 import GHC.Exts
@@ -19,20 +19,34 @@ main
  = do   args <- getArgs
         let sz = case args of
                    [szStr] -> (Prelude.read szStr :: Int)
-                   _       -> error "Usage: nestedfilter <size>"
-        v1      <- V.fromUnboxed $ U.enumFromN (0 :: Int) sz
-        v1 `seq` return ()
-        ((v,u),t) <- time $ let (v,u) = R.runSeries v1 lower_nested
-                              in v `seq` u `seq` return (v,u)
+                   _       -> error "Usage: nestedFilter <size>"
+
+        v0      <- V.fromPrimitive $ P.enumFromN (0 :: Int) sz
+        v1      <- V.new sz
+        v2      <- V.new sz
+
+        v0 `seq` v1 `seq` v2 `seq` return ()
+        ((),t)  <- time $  R.runProcess v0 (nestedFilter v1 v2)
+
+        p1      <- V.toPrimitive v1
+        p2      <- V.toPrimitive v2
+
         putStr	$ prettyTime t
-        print (I# (V.length v), I# (V.length u))
+        print (P.length p1, P.length p2)
 
-lower_nested :: R.Series k Int -> (R.Vector Int, R.Vector Int)
-lower_nested xs
- = let xsS = R.map (\x -> x > 50) xs
-   in  R.mkSel1 xsS (\selX ->
-       let ys  = R.pack selX xs
-           ysS = R.map (\x -> x < 100) ys
-       in  R.mkSel1 ysS (\selY ->
-           (S.toVector ys, S.toVector (R.pack selY ys))))
 
+nestedFilter 
+        :: R.Vector Int -> R.Vector Int
+        -> R.RateNat k  -> R.Series k Int
+        -> Process
+
+nestedFilter v1 v2 _ s0
+ = let fs1      = R.map (\x -> x > 50) s0
+   in  R.mkSel1 fs1 (\sel1 ->
+        let s1   = R.pack sel1 s0
+            fs2  = R.map (\x -> x < 100) s1
+        in R.mkSel1 fs2 (\sel2 ->
+            let s2      = R.pack sel2 s1
+            in  R.fill v1 s1
+             %  R.fill v2 s2))
+{-# NOINLINE nestedFilter #-}
