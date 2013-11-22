ab6379741b7c27a119f13385dfd453e645bdbde4
Ben Lippmeier <benl@ouroborus.net>
2013-11-22 16:19:43 +1100

    plugin: update FilterMax test to new api
    Ignore-this: 545c4ad8b384fb31afaef083b1f95650
    
    darcs-hash:20131122051943-08f32-d9d5917e31b2e8e72020017177cb68900cfc55bd

--- repa-plugin/test/90-Benchmarks/repa/FilterMax/Main.hs
+++ repa-plugin/test/90-Benchmarks/repa/FilterMax/Main.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE BangPatterns #-}
 module Main where
-import Data.Array.Repa.Series           as R
-import Data.Array.Repa.Series.Series    as S
-import Data.Array.Repa.Series.Vector    as V
-import Data.Array.Repa.IO.Timing        as R
-import qualified Data.Vector.Unboxed    as U
+import Data.Array.Repa.Series                   as R
+import Data.Array.Repa.Series.Series            as S
+import Data.Array.Repa.Series.Vector            as V
+import Data.Array.Repa.IO.Timing                as R
+import qualified Data.Array.Repa.Series.Ref     as Ref
+import qualified Data.Vector.Primitive          as P
+import Data.Array.Repa.Series.Ref               (Ref)
 import GHC.Exts
 import System.Environment
 
@@ -22,53 +24,60 @@ main
         let (alg, sz)  
                  = case args of
                      [alg', szStr] -> (alg', Prelude.read szStr :: Int)
-                     _             -> error "Usage: simplebench <alg> <size>"
+                     _             -> error "Usage: filterMax <alg> <size>"
 
-        let !uvec = U.enumFromN (0 :: Int) sz
+        let !vec = P.enumFromN (0 :: Int) sz
 
-        ((uvec', n1), t) 
-         <- R.time $ do (uvec', n1) <- run alg uvec
-                        uvec' `seq` n1 `seq` return (uvec', n1)
+        ((vec', n1), t) 
+         <- R.time $ do (vec', n1) <- run alg vec
+                        vec' `seq` n1 `seq` return (vec', n1)
 
         print n1
         putStr (prettyTime t)
 
-run "vector" uvec = run_vector uvec
-run "flow"   uvec = run_flow   uvec
+run "vector" vec = run_vector vec
+run "flow"   vec = run_flow   vec
         
 
 -------------------------------------------------------------------------------
-run_vector :: U.Vector Int  -> IO (U.Vector Int, Int)
-run_vector uvec1
- = let  uvec2   = U.map (+ 1) uvec1
-        uvec4   = U.filter (\x -> x > 0) uvec2
-        n1      = U.foldl maxx 0 uvec4
-   in   return  (uvec4, n1)
+run_vector :: P.Vector Int  -> IO (P.Vector Int, Int)
+run_vector vec1
+ = let  vec2   = P.map (+ 1) vec1
+        vec4   = P.filter (\x -> x > 0) vec2
+        n1     = P.foldl maxx 0 vec4
+   in   return  (vec4, n1)
 {-# NOINLINE run_vector #-}
 
 
-run_flow  :: U.Vector Int -> IO (U.Vector Int, Int)
-run_flow uvec
- = do   vvec2   <- V.fromUnboxed uvec
-        let (vvec3, n1) = R.runSeries vvec2 lower_maxx
-        uvec3   <- V.toUnboxed vvec3
-        return  (uvec3, n1)
+run_flow  :: P.Vector Int -> IO (P.Vector Int, Int)
+run_flow pvec
+ = do   vec     <- V.fromPrimitive pvec
+        vec'    <- V.new (P.length pvec)
+        rMax    <- Ref.new 0 
+        R.runProcess vec (filterMax vec' rMax)
+        mx      <- Ref.read rMax
+        pvec'   <- V.toPrimitive vec'
+        return  (pvec', mx)
 {-# NOINLINE run_flow #-}
-
 
 
 -- | Get the vector of positive values,
 --   as well as the maximual element.
-lower_maxx :: Series k Int -> (Vector Int, Int)
-lower_maxx s1
+filterMax 
+        :: Vector Int -> Ref Int
+        -> RateNat k  -> Series k Int
+        -> Process
+
+filterMax v r _ s1
  = let  s2      = R.map (\x -> x + 1) s1
         s3      = R.map (\x -> x > 0) s2
    in   R.mkSel1 s3 
          (\sel -> let s4   = R.pack sel s2
-                  in  ( S.toVector s4
-                      , R.fold maxx 0 s4))
+                  in  R.fill v s4
+                   %  R.reduce r maxx 0 s4)
 
 maxx :: Int -> Int -> Int
 maxx x y
  = if x > y then x else y
 {-# INLINE [0] maxx #-}
+
