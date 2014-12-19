74d57dc8dcf5b883132723f5965a56b16954fa4b
Ben Lippmeier <benl@ouroborus.net>
2014-12-19 16:27:42 +1100

    flow: start on record reader

--- repa-bulk/Data/Array/Repa/Bulk/IO/File.hs
+++ repa-bulk/Data/Array/Repa/Bulk/IO/File.hs
@@ -1,38 +1,58 @@
 
 module Data.Array.Repa.Bulk.IO.File
-        ( hGetArrayF
+        ( hGetArrayF,   hGetArrayPreF
         , hPutArrayF)
 where
 import Data.Array.Repa.Repr.Foreign
 import Data.Array.Repa.Bulk
-import Foreign.Ptr
-import Foreign.ForeignPtr
-import Foreign.Marshal.Alloc
+import qualified Foreign.Ptr            as F
+import qualified Foreign.ForeignPtr     as F
+import qualified Foreign.Marshal.Alloc  as F
+import qualified Foreign.Marshal.Utils  as F
 import System.IO
 import Data.Word
 
 
 -- | Get data from a file, up to the given number of bytes.
 -- 
---   Data read from the file ends up in foreign memory, 
---   which can then be used without copying it into the GHC heap.
+--   * Data is read into foreign memory without copying it through the GHC heap.
+--
 hGetArrayF :: Handle -> Int -> IO (Vector F Word8)
 hGetArrayF h len
  = do
-        buf :: Ptr Word8 <- mallocBytes len
-        bytesRead        <- hGetBuf h buf len
-        fptr             <- newForeignPtr finalizerFree buf
+        buf :: F.Ptr Word8 <- F.mallocBytes len
+        bytesRead          <- hGetBuf h buf len
+        fptr               <- F.newForeignPtr F.finalizerFree buf
         return  $ fromForeignPtr (Z :. bytesRead) fptr
 {-# NOINLINE hGetArrayF #-}
 
 
+-- | Get data from a file, up to the given number of bytes, also 
+--   copying the given data to the front of the new buffer.
+--
+--   * Data is read into foreign memory without copying it through the GHC heap.
+--
+hGetArrayPreF :: Handle -> Int -> Vector F Word8 -> IO (Vector F Word8)
+hGetArrayPreF h len (FArray _ lenPre fptrPre)
+ = F.withForeignPtr fptrPre
+ $ \ptrPre -> do   
+        ptrBuf :: F.Ptr Word8 <- F.mallocBytes (lenPre + len)
+        F.copyBytes ptrBuf ptrPre lenPre
+        lenRead               <- hGetBuf h (F.plusPtr ptrBuf lenPre) len
+        let bytesTotal  = lenPre + lenRead
+        fptrBuf               <- F.newForeignPtr F.finalizerFree ptrBuf
+        return  $ fromForeignPtr (Z :. bytesTotal) fptrBuf
+{-# NOINLINE hGetArrayPreF #-}
+
+
 -- | Write data into a file.
 --
---   Data is written to file directly from foreign memory,
---   without copying it through the GHC heap.
+--   * Data is written to file directly from foreign memory,
+--     without copying it through the GHC heap.
+--
 hPutArrayF :: Handle -> Vector F Word8 -> IO ()
 hPutArrayF h (FArray _ len fptr)
- = withForeignPtr fptr
+ = F.withForeignPtr fptr
  $ \ptr -> do
         hPutBuf h ptr len
 {-# NOINLINE hPutArrayF #-}
--- repa-flow/Data/Array/Repa/Flow/IO/File.hs
+++ repa-flow/Data/Array/Repa/Flow/IO/File.hs
@@ -1,31 +1,36 @@
 
 module Data.Array.Repa.Flow.IO.File
-        ( fileSourceBytesUF, hSourceBytesUF
-        , fileSinkBytesUF,   hSinkBytesUF)
+        ( fileSourceBytesF, hSourceBytesF
+                          , hSourceRecordsF
+        , fileSinkBytesF,   hSinkBytesF)
 where
 import Data.Array.Repa.Flow.Internals.Base
-import Data.Array.Repa.Repr.Unsafe.Foreign
+import Data.Array.Repa.Repr.Foreign
 import Data.Array.Repa.Bulk.IO.File
 import Data.Array.Repa.Bulk
 import System.IO
 import Data.Word
+import Data.IORef
 
 
+-- Source -----------------------------------------------------------------------------------------
 -- | Read chunks of data of the given size from a file.
 --
---   All chunks have the same size, except possibly the last one.
+--   * Data is read into foreign memory without copying it through the GHC heap.
+--   * All chunks have the same size, except possibly the last one.
 --
 --   TODO: close file when finished.
 fileSourceBytesF :: FilePath -> Int -> IO (Source (Vector F Word8))
 fileSourceBytesF filePath len
  = do   h       <- openBinaryFile filePath ReadMode
-        hSourceBytesUF h len 
-{-# INLINE [2] fileSourceBytesUF #-}
+        hSourceBytesF h len 
+{-# INLINE [2] fileSourceBytesF #-}
 
 
 -- | Read chunks of data of the given size from the file handle.
 --
---   All chunks have the same size, except possibly the last one.
+--   * Data is read into foreign memory without copying it through the GHC heap.
+--   * All chunks have the same size, except possibly the last one.
 --
 hSourceBytesF :: Handle -> Int -> IO (Source (Vector F Word8))
 hSourceBytesF h len
@@ -35,34 +40,81 @@ hSourceBytesF h len
          = do eof <- hIsEOF h
               if eof 
                then     eject
-               else do  !chunk  <- hGetArrayUF h len
+               else do  !chunk  <- hGetArrayF h len
                         eat chunk
         {-# INLINE pull_hSource #-}
-{-# INLINE [2] hSourceBytesUF #-}
+{-# INLINE [2] hSourceBytesF #-}
 
 
+-- | Read complete records of data from a file handle, 
+--   up to the given size in bytes.
+--
+--   The records are separated with the given terminating character.
+--   After reading some records from the file we seek to just after
+--   the last record which was read, so we can continue to read
+--   more complete records next time.
+--
+--   * Data is read into foreign memory without copying it through the GHC heap.
+--   * All chunks have the same size, except possibly the last one.
+--   * The provided file handle must support seeking, else you'll get an exception.
+-- 
+hSourceRecordsF :: Handle -> Int -> Word8 -> IO (Source (Vector F Word8))
+hSourceRecordsF h len _sep 
+ = do
+        rSpill  <- newIORef Nothing
+
+        let pull_hSourceRecordsF eat eject
+             = do eof <- hIsEOF h
+                  if eof
+                   then do
+                        mspill   <- readIORef rSpill
+                        case mspill of
+                         Nothing    -> eject
+                         Just chunk -> do _ <- eat chunk
+                                          eject
+
+                   else do
+                        mspill   <- readIORef rSpill
+                        case mspill of
+                         Nothing    
+                          -> do arr <- hGetArrayF h len
+                                eat arr
+
+                         Just arrSpill
+                          -> do arr <- hGetArrayPreF h len arrSpill
+                                eat arr
+            {-# INLINE pull_hSourceRecordsF #-}
+
+        return $ Source pull_hSourceRecordsF
+{-# INLINE [2] hSourceRecordsF #-}
+
+
+-- Sink -------------------------------------------------------------------------------------------
 -- | Write chunks of data to the given file.
 --
 --   TODO: close file when finished.
 --
 fileSinkBytesF
-        :: FilePath -> IO (Sink (Vector UF Word8))
+        :: FilePath -> IO (Sink (Vector F Word8))
 
 fileSinkBytesF filePath
  = do   h       <- openBinaryFile filePath WriteMode
-        hSinkBytesUF h
+        hSinkBytesF h
+{-# INLINE [2] fileSinkBytesF #-}
 
 
 -- | Write chunks of data to the given file handle.
 hSinkBytesF :: Handle -> IO (Sink (Vector F Word8))
 hSinkBytesF !h
- = do   let push_hSinkBytesUF !chunk
-                = hPutArrayUF h chunk
-            {-# NOINLINE push_hSinkBytesUF #-}
+ = do   let push_hSinkBytesF !chunk
+                = hPutArrayF h chunk
+            {-# NOINLINE push_hSinkBytesF #-}
 
-        let eject_hSinkBytesUF
+        let eject_hSinkBytesF
                 = return ()
-            {-# INLINE eject_hSinkBytesUF #-}
+            {-# INLINE eject_hSinkBytesF #-}
 
-        return  $ Sink push_hSinkBytesUF eject_hSinkBytesUF
-{-# INLINE [2] hSinkBytesUF #-}
+        return  $ Sink push_hSinkBytesF eject_hSinkBytesF
+{-# INLINE [2] hSinkBytesF #-}
+
+
