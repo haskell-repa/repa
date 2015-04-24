f7756f21ed8c9ba589c5856174874e1fd78738c6
Ben Lippmeier <benl@ouroborus.net>
2015-04-24 21:58:47 +1000

    array: split out auto array base defs into own module

--- repa-flow/Data/Repa/Flow/Auto.hs
+++ repa-flow/Data/Repa/Flow/Auto.hs
@@ -60,6 +60,7 @@ module Data.Repa.Flow.Auto
         , finalize_i,           finalize_o
         )
 where
+import Data.Repa.Flow.Auto.Base
 import Data.Repa.Array.Auto                    
         hiding (fromList, fromLists)
 
@@ -75,29 +76,6 @@ import Control.Monad
 #include "repa-flow.h"
 
 
--- | A bundle of stream sources, where the elements of the stream
---   are chunked into arrays.
-type Sources a  = C.Sources Int IO A a
-
-
--- | A bundle of stream sinks,   where the elements of the stream
---   are chunked into arrays.
---
-type Sinks a    = C.Sinks Int IO A a
-
-
--- | Yield the number of streams in the bundle.
-sourcesArity :: Sources a -> Int
-sourcesArity = G.sourcesArity
-
-
--- | Yield the number of streams in the bundle.
-sinksArity :: Sinks a -> Int
-sinksArity = G.sinksArity
-
-
--- | Shorthand for common type classes.
-type Flow a     = (C.Flow  Int IO A a, A.Windowable A a)
 
 
 -- Evaluation -----------------------------------------------------------------
@@ -123,44 +101,6 @@ drainP   :: Sources a -> Sinks a -> IO ()
 drainP = G.drainP
 {-# INLINE drainP #-}
 
-
--- Conversion -----------------------------------------------------------------
--- | Given an arity and a list of elements, yield sources that each produce all
---   the elements. 
---
---   * All elements are stuffed into a single chunk, and each stream is given
---     the same chunk.
---
-fromList :: Build a t
-         => Int -> [a] -> IO (Sources a)
-fromList xs = C.fromList A xs
-{-# INLINE fromList #-}
-
-
--- | Like `fromLists_i` but take a list of lists. Each each of the inner
---   lists is packed into a single chunk.
-fromLists :: Build a t
-          => Int -> [[a]] -> IO (Sources a)
-fromLists xss = C.fromLists A xss
-{-# INLINE fromLists #-}
-
-
--- | Drain a single source from a bundle into a list of elements.
-toList1   :: Build a t
-          => Int -> Sources a -> IO [a]
-toList1 ix s  
- | ix >= G.sourcesArity s = return []
- | otherwise              = C.toList1 ix s 
-{-# INLINE toList1 #-}
-
-
--- | Drain a single source from a bundle into a list of chunks.
-toLists1  :: Build a t
-          => Int -> Sources a -> IO [[a]]
-toLists1 ix s
- | ix >= G.sourcesArity s = return []
- | otherwise              = C.toLists1 ix s 
-{-# INLINE toLists1 #-}
 
 
 -- Finalizers -----------------------------------------------------------------
---
+++ repa-flow/Data/Repa/Flow/Auto/Select.hs
@@ -0,0 +1,12 @@
+
+module Data.Repa.Flow.Chunked.Select
+where
+import Data.Repa.Flow.Chunked.Base      as F
+import Data.Repa.Singleton.Nat          as D
+import Data.Repa.Product                as D
+#include "repa-array.h"
+
+
+select_i 
+        :: Sources 
+        
